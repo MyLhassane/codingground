0bc65bda357f2d2a1f81239a69787e78741b539e
mastinoz <mastinoz_7@hotmail.com>
2016-08-16 06:09:01 +0100

    Created by Coding Ground Client

---
+++ New Project-20160816/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+         int    x   [];
+         
+         x  =   new int [3];
+         x[0]   =   5;
+         x[1]   =   9;
+         x[2]   =   65;
+         
+         
+        System.out.println(x[2]);
+     }
+}
